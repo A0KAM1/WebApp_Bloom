fe71223bcce2bb5d27d1db0a7b4a91d7043aa78d
mariacandido-droid <maricandido75777@gmail.com>
2023-05-02 13:37:20 -0300

    Criação da Lista de convidados

---
+++ WebApp_Bloom/Controllers/ListaConvidados.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Bloom.Controllers
+{
+    public class ListaConvidados : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
