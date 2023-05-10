3d24e9293b2592c4660549bfa0e63089f857936e
A0KAM1 <leticiarodrigues1603@hotmail.com>
2023-05-10 17:48:58 -0300

    Muitas coisas

--- WebApp_Bloom/Controllers/EventoController.cs
+++ WebApp_Bloom/Controllers/EventoController.cs
@@ -4,15 +4,7 @@ namespace WebApp_Bloom.Controllers
 {
     public class EventoController : Controller
     {
-        public IActionResult SelecionarEvento()
-        {
-            return View();
-        }
-        public IActionResult Casamento()
-        {
-            return View();
-        }
-        public IActionResult Aniversario()
+        public IActionResult Cadastrar()
         {
             return View();
         }
