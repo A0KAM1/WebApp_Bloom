2d3693896763df403e997e9cc5981e5d6b7721d4
mariacandido-droid <maricandido75777@gmail.com>
2023-05-22 16:28:31 -0300

    Adicionei controller de fornecedor, Entidade de casamento e Fornecedor, Arrumei as views de editar e listar de convidados e fornecedor.

---
+++ WebApp_Bloom/Controllers/FornecedorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_Bloom.Entidades;
+
+namespace WebApp_Bloom.Controllers
+{
+    public class FornecedorController : Controller
+    {
+        public IActionResult Lista()
+        {
+            return View();
+        }
+        public IActionResult Editar(FornecedorEntidade fornecedor ) {
+            if (fornecedor != null)
+            {
+                return View(fornecedor);
+            }
+            else
+            {
+                return RedirectToAction("Lista");
+            }
+        }
+    }
+}
