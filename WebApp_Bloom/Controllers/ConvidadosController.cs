d89ddd604d6e8d0e0ffc1fd3869c23141f5fa373
mariacandido-droid <maricandido75777@gmail.com>
2023-05-22 15:50:19 -0300

    -> Adicionei o caminho e a tela de editar nome do convidado. -> Criei a classe de Model, Contexto.

--- WebApp_Bloom/Controllers/ConvidadosController.cs
+++ WebApp_Bloom/Controllers/ConvidadosController.cs
@@ -1,12 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_Bloom.Entidades;
 
 namespace WebApp_Bloom.Controllers
 {
     public class ConvidadosController : Controller
     {
+        
         public IActionResult Lista()
         {
             return View();
+        }
+        public IActionResult Editar( ConvidadosEntidade convidado) {
+
+            if (convidado != null)
+            {
+                return View(convidado);
+            }
+            else
+            {
+                return RedirectToAction("Lista");
+            }
         }
     }
 }
